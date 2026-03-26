a3e5e9af3e15171007185bd4b438b541ac921c00
Exathi <exathi.github@gmail.com>
2026-03-25 22:28:29 -0400

    update examples with browsercontext and perform the same actions.

--- Example Async.ps1
+++ Example Async.ps1
@@ -5,41 +5,38 @@ $UriBuilder = [System.UriBuilder]::new($StartPage)
 $UserDataDir = 'D:\The Testing Folder\Edge\TestUserData'
 $BrowserPath = 'C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe'
 
-# 3 additional threads to run each $Async in the runspacepool.
-# last one is queued since there are not enough runspaces in the pool.
-$Server = Start-CdpServer -StartPage $UriBuilder.Uri.AbsoluteUri -UserDataDir $UserDataDir -BrowserPath $BrowserPath -AdditionalThreads 3
+# If there are not enough runspaces in the pool they will be queued.
+# If all runspaces are exhausted with StartMessageProcessor/StartMessageWriter the $Async will never run.
+$Server = Start-CdpServer -StartPage $UriBuilder.Uri.AbsoluteUri -UserDataDir $UserDataDir -BrowserPath $BrowserPath -AdditionalThreads 4
 
 $TestClickType = {
     param($Server)
-    $CdpPage = New-CdpPage -Server $Server -Url 'https://www.selenium.dev/selenium/web/single_text_input.html' -NewWindow
-    Invoke-CdpPageNavigate -Server $Server -SessionId $CdpPage.SessionId -Url 'https://www.selenium.dev/selenium/web/single_text_input.html'
-    Invoke-CdpInputClickElement -Server $Server -SessionId $CdpPage.SessionId -Selector 'document.querySelector("[id=textInput]")' -Click 1
-    Invoke-CdpInputSendKeys -Server $Server -SessionId $CdpPage.SessionId -Keys 'Hello World'
-    Invoke-CdpInputClickElement -Server $Server -SessionId $CdpPage.SessionId -Selector 'document.querySelector("#textInput")' -Click 3 -TopLeft
-    Invoke-CdpInputSendKeys -Server $Server -SessionId $CdpPage.SessionId -Keys 'PSChromeDevToolsServer'
-
-    'Finished TestClickType'
-}.Ast.GetScriptBlock()
-
-$TestClickFrame = {
-    param($Server)
+    # MUST BE WINDOWED NOT FULLSCREENED
     # We use -NewWindow because sometimes inputs do not register to a tab that is not active.
-    # It does work if tabs are on separate windows.
+    # It works if tabs are on separate windows.
     # Alternatively use javascript.click()
-    $CdpPage = New-CdpPage -Server $Server -Url 'https://www.selenium.dev/selenium/web/click_frames.html' -NewWindow
-    Invoke-CdpInputClickElement -Server $Server -SessionId $CdpPage.SessionId -Selector 'document.querySelector("frameset frame").contentDocument.querySelector("[id=source]").contentDocument.querySelector("[id=otherframe]")' -Click 1
+    $CdpPage = New-CdpPage -Server $Server -Url 'https://www.selenium.dev/selenium/web/single_text_input.html' -NewWindow
+    Invoke-CdpPageNavigate -Server $Server -SessionId $CdpPage.TargetInfo.SessionId -Url 'https://www.selenium.dev/selenium/web/single_text_input.html'
+    Invoke-CdpInputClickElement -Server $Server -SessionId $CdpPage.TargetInfo.SessionId -Selector 'document.querySelector("[id=textInput]")' -Click 1
+    Invoke-CdpInputSendKeys -Server $Server -SessionId $CdpPage.TargetInfo.SessionId -Keys 'Hello World'
+    Invoke-CdpInputClickElement -Server $Server -SessionId $CdpPage.TargetInfo.SessionId -Selector 'document.querySelector("#textInput")' -Click 3 -TopLeft
+    Invoke-CdpInputSendKeys -Server $Server -SessionId $CdpPage.TargetInfo.SessionId -Keys 'PSChromeDevToolsServer'
+    'Finished TestClickType'
 
+    $CdpPage2 = New-CdpPage -Server $Server -Url 'https://www.selenium.dev/selenium/web/click_frames.html' -BrowserContextId $CdpPage.BrowserContextId
+    Invoke-CdpInputClickElement -Server $Server -SessionId $CdpPage2.TargetInfo.SessionId -Selector 'document.querySelector("frameset frame").contentDocument.querySelector("[id=source]").contentDocument.querySelector("[id=otherframe]")' -Click 1
     'Finished TestClickFrame'
 }.Ast.GetScriptBlock()
 
 $Async1 = [powershell]::Create().AddScript($TestClickType).AddParameter('Server', $Server)
-$Async2 = [powershell]::Create().AddScript($TestClickFrame).AddParameter('Server', $Server)
-$Async3 = [powershell]::Create().AddScript($TestClickFrame).AddParameter('Server', $Server)
+$Async2 = [powershell]::Create().AddScript($TestClickType).AddParameter('Server', $Server)
+$Async3 = [powershell]::Create().AddScript($TestClickType).AddParameter('Server', $Server)
 $Async4 = [powershell]::Create().AddScript($TestClickType).AddParameter('Server', $Server)
 $Async1.RunspacePool = $Server.Runspacepool
 $Async2.RunspacePool = $Server.Runspacepool
 $Async3.RunspacePool = $Server.Runspacepool
 $Async4.RunspacePool = $Server.Runspacepool
+
 $Handle1 = $Async1.BeginInvoke()
 $Handle2 = $Async2.BeginInvoke()
 $Handle3 = $Async3.BeginInvoke()
@@ -58,22 +55,9 @@ $Task3 = [System.Threading.Tasks.Task]::Factory.FromAsync($Handle3, $EndInvokeDe
 $Task4 = [System.Threading.Tasks.Task]::Factory.FromAsync($Handle4, $EndInvokeDelegate4)
 
 $WaitAll = [System.Threading.Tasks.Task]::WhenAll($Task1, $Task2, $Task3, $Task4)
-$WaitAll.GetAwaiter().GetResult()
-
-$Task1.Dispose()
-$Task2.Dispose()
-$Task3.Dispose()
-$Task4.Dispose()
-
-$Handle1 = $Async1.BeginInvoke()
-$Handle2 = $Async2.BeginInvoke()
-$Handle3 = $Async3.BeginInvoke()
-$Handle4 = $Async4.BeginInvoke()
-$Task1 = [System.Threading.Tasks.Task]::Factory.FromAsync($Handle1, $EndInvokeDelegate1)
-$Task2 = [System.Threading.Tasks.Task]::Factory.FromAsync($Handle2, $EndInvokeDelegate2)
-$Task3 = [System.Threading.Tasks.Task]::Factory.FromAsync($Handle3, $EndInvokeDelegate3)
-$Task4 = [System.Threading.Tasks.Task]::Factory.FromAsync($Handle4, $EndInvokeDelegate4)
-$WaitAll = [System.Threading.Tasks.Task]::WhenAll($Task1, $Task2, $Task3, $Task4)
+while (!$WaitAll.GetAwaiter().IsCompleted) {
+    Start-Sleep -Milliseconds 1 # for ctrl + c
+}
 $WaitAll.GetAwaiter().GetResult()
 
 # $Server.ShowMessageHistory() | Format-Table -AutoSize
--- Example Sync.ps1
+++ Example Sync.ps1
@@ -5,22 +5,16 @@ $UriBuilder = [System.UriBuilder]::new($StartPage)
 $UserDataDir = 'D:\The Testing Folder\Edge\TestUserData'
 $BrowserPath = 'C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe'
 
-$Server = Start-CdpServer -StartPage $UriBuilder.Uri.AbsoluteUri -UserDataDir $UserDataDir -BrowserPath $BrowserPath
+$Server = Start-CdpServer -StartPage $UriBuilder.Uri.AbsoluteUri -UserDataDir $UserDataDir -BrowserPath $BrowserPath -Debug
 
-$FirstTab = $Server.SharedState.Targets.Values[0]
-Invoke-CdpPageNavigate -Server $Server -SessionId $FirstTab.SessionId -Url 'https://www.selenium.dev/selenium/web/click_frames.html'
-Invoke-CdpInputClickElement -Server $Server -SessionId $FirstTab.SessionId -Selector 'document.querySelector("frameset frame").contentDocument.querySelector("[id=source]").contentDocument.querySelector("[id=otherframe]")' -Click 1
-
-Start-Sleep -Seconds 1
-
-Invoke-CdpPageNavigate -Server $Server -SessionId $FirstTab.SessionId -Url 'https://www.selenium.dev/selenium/web/single_text_input.html'
-Invoke-CdpInputClickElement -Server $Server -SessionId $FirstTab.SessionId -Selector 'document.querySelector("[id=textInput]")' -Click 1
-Invoke-CdpInputSendKeys -Server $Server -SessionId $FirstTab.SessionId -Keys 'Hello World'
-Invoke-CdpInputClickElement -Server $Server -SessionId $FirstTab.SessionId -Selector 'document.querySelector("#textInput")' -Click 3 -TopLeft
-Invoke-CdpInputSendKeys -Server $Server -SessionId $FirstTab.SessionId -Keys 'PSChromeDevToolsServer'
-
-$SecondTab = New-CdpPage -Server $Server -Url 'https://www.selenium.dev/selenium/web/click_frames.html'
-Invoke-CdpInputClickElement -Server $Server -SessionId $SecondTab.SessionId -Selector 'document.querySelector("frameset frame").contentDocument.querySelector("[id=source]").contentDocument.querySelector("[id=otherframe]")' -Click 1
+$CdpPage = New-CdpPage -Server $Server -Url 'https://www.selenium.dev/selenium/web/single_text_input.html' -NewWindow
+Invoke-CdpPageNavigate -Server $Server -SessionId $CdpPage.TargetInfo.SessionId -Url 'https://www.selenium.dev/selenium/web/single_text_input.html'
+Invoke-CdpInputClickElement -Server $Server -SessionId $CdpPage.TargetInfo.SessionId -Selector 'document.querySelector("[id=textInput]")' -Click 1
+Invoke-CdpInputSendKeys -Server $Server -SessionId $CdpPage.TargetInfo.SessionId -Keys 'Hello World'
+Invoke-CdpInputClickElement -Server $Server -SessionId $CdpPage.TargetInfo.SessionId -Selector 'document.querySelector("#textInput")' -Click 3 -TopLeft
+Invoke-CdpInputSendKeys -Server $Server -SessionId $CdpPage.TargetInfo.SessionId -Keys 'PSChromeDevToolsServer'
+$CdpPage2 = New-CdpPage -Server $Server -Url 'https://www.selenium.dev/selenium/web/click_frames.html' -BrowserContextId $CdpPage.BrowserContextId
+Invoke-CdpInputClickElement -Server $Server -SessionId $CdpPage2.TargetInfo.SessionId -Selector 'document.querySelector("frameset frame").contentDocument.querySelector("[id=source]").contentDocument.querySelector("[id=otherframe]")' -Click 1
 
 $Server.ShowMessageHistory() | Format-Table -AutoSize
 
