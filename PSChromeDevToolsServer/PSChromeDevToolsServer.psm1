317d657ce764361313bbfb14dabcf79a11d6ab1c
Exathi <exathi.github@gmail.com>
2026-04-03 23:54:59 -0400

    add workaround for rare case where runtime.evaluate could use an old RuntimeUniqueId

--- PSChromeDevToolsServer/PSChromeDevToolsServer.psm1
+++ PSChromeDevToolsServer/PSChromeDevToolsServer.psm1
@@ -1283,6 +1283,16 @@ function Invoke-CdpInputClickElement {
 		$Command = Get-Runtime.evaluate $SessionId $Selector
 		$Command.params.uniqueContextId = "$($CdpPage.PageInfo['RuntimeUniqueId'])"
 		$Response = $CdpServer.SendCommand($Command, [WaitForResponse]::Message)
+
+		if ($Response.error.code -eq '-32602') {
+			# message=uniqueContextId not found
+			# Wait for new context in rare cases because Runtime.executionContextCreated has not been processed for the new page but Runtime.evaluate is called.
+			$NewId = $null
+			[System.Threading.SpinWait]::SpinUntil({ $CdpPage.PageInfo.TryGetValue('RuntimeUniqueId', [ref]$NewId); $NewId -ne $Command.params.uniqueContextId })
+			$Command.params.uniqueContextId = "$NewId"
+			$Response = $CdpServer.SendCommand($Command, [WaitForResponse]::Message)
+		}
+
 		$CdpPage.PageInfo['ObjectId'] = $Response.result.result.objectId
 
 		if ($Click -le 0) { return $_ }
@@ -1449,6 +1459,15 @@ awaitMultiplePromises();
 		$Command.params.uniqueContextId = "$($CdpPage.PageInfo['RuntimeUniqueId'])"
 		if ($AwaitPromise) { $Command.params.awaitPromise = $true }
 		$Response = $CdpServer.SendCommand($Command, [WaitForResponse]::Message)
+
+		if ($Response.error.code -eq '-32602') {
+			# message=uniqueContextId not found
+			# Wait for new context in rare cases because Runtime.executionContextCreated has not been processed for the new page but Runtime.evaluate is called.
+			$NewId = $null
+			[System.Threading.SpinWait]::SpinUntil({ $CdpPage.PageInfo.TryGetValue('RuntimeUniqueId', [ref]$NewId); $NewId -ne $Command.params.uniqueContextId })
+			$Command.params.uniqueContextId = "$NewId"
+			$Response = $CdpServer.SendCommand($Command, [WaitForResponse]::Message)
+		}
 
 		$CdpPage.PageInfo['EvaluateResult'] = $Response.result.result
 		$CdpPage.PageInfo['EvaluateResponse'] = $Response
